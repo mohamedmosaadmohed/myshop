170dd92a828ac1f9fac40274d2e930ddc836d99f
mohamed mosaad <103392388+mohamedmosaadmohed@users.noreply.github.com>
2024-07-03 09:35:54 +0300

    Create OrderPickUp Details

--- myshop.Web/Areas/Admin/Controllers/OrderController.cs
+++ myshop.Web/Areas/Admin/Controllers/OrderController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using myshop.Entities.Models;
 using myshop.Entities.Repositories;
+using myshop.Entities.ViewModels;
+using myshop.Utilities;
 
 namespace myshop.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles =SD.AdminRole)]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -22,6 +26,15 @@ namespace myshop.Web.Areas.Admin.Controllers
             IEnumerable<OrderHeader> OrderHeaders;
             OrderHeaders = _unitOfWork.OrderHeader.GetAll(IncludeWord: "applicationUser");
             return Json(new {data = OrderHeaders});
+        }
+        public IActionResult Details(int orderid)
+        {
+            OrderVM orderVM = new OrderVM()
+            {
+                orderHeader =_unitOfWork.OrderHeader.GetFirstorDefault(X => X.Id == orderid, IncludeWord: "applicationUser"),
+                orderDetails =_unitOfWork.OrderDetails.GetAll(X=> X.OrderHeaderId == orderid , IncludeWord: "product"),
+            };
+            return View(orderVM);
         }
     }
 }
