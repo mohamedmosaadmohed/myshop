432a03a7c37eb6247f0e4ec626691e96c59fb429
mohamed mosaad <103392388+mohamedmosaadmohed@users.noreply.github.com>
2024-07-03 08:48:25 +0300

    Create DataTable for OrderHeader

--- myshop.Web/Areas/Admin/Controllers/OrderController.cs
+++ myshop.Web/Areas/Admin/Controllers/OrderController.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using myshop.Entities.Models;
+using myshop.Entities.Repositories;
 
 namespace myshop.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class OrderController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            IEnumerable<OrderHeader> OrderHeaders;
+            OrderHeaders = _unitOfWork.OrderHeader.GetAll(IncludeWord: "applicationUser");
+            return Json(new {data = OrderHeaders});
         }
     }
 }
---
+++ myshop.Web/wwwroot/js/Order.js
@@ -0,0 +1,68 @@
+﻿var dtble;
+$(document).ready(function () {
+    loaddata();
+});
+
+function loaddata() {
+    dtble = $("#mytable").DataTable({
+        "filter": true,
+        "ajax": {
+            "url": "/Admin/Order/GetData"
+        },
+        "type": "GET",
+        "datatype": "json",
+        "columns": [
+            { "data": "id" },
+            {
+                "data": null,
+                "render": function (data, type, row) {
+                    return row.firstName + ' ' + row.lastName;
+                },
+                "title": "name"
+            },
+            { "data": "phoneNumber" },
+            { "data": "orderStatus" },
+            { "data": "totalPrice" },
+            { "data": "applicationUser.email" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<a href="/Admin/Order/Details?orderid=${data}" class="btn btn-warning">Details</a>`;
+                },
+                "orderable": false
+            }
+        ]
+    });
+}
+
+function DeleteItem(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    if (data.success) {
+                        dtble.ajax.reload();
+                        toastr.success(data.message);
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+            Swal.fire({
+                title: "Deleted!",
+                text: "Your product has been deleted.",
+                icon: "success"
+            });
+        }
+    });
+}
